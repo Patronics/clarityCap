419f5ebf5b2d554bfa6feb5766342bb8f242e498
ThePie(ProbablyKyleOrPatrick) <piePi@cap2.tech>
2024-04-27 23:21:58 -0700

    start of web interface and facial detection progress

--- facial_detection/demo.py
+++ facial_detection/demo.py
@@ -15,12 +15,8 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Create arrays of known face encodings and their names
-known_face_encodings = [
-    
-]
-known_face_names = [
-    
-]
+known_face_encodings = []
+known_face_names = []
 
 known_image = face_recognition.load_image_file("facial_detection/knownPeople/kyle.jpg")
 
@@ -35,10 +31,13 @@ process_this_frame = True
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
+
+    frame = cv2.resize(frame, (0, 0), fx=0.2, fy=0.2)
+    # frame = frame[:, :, ::-1]
     
     face_locations = face_recognition.face_locations(frame)
+    image_face_encodings = face_recognition.face_encodings(frame, face_locations)
 
-    # unknown_encoding = face_recognition.face_encodings(frame)[0]
     # results = face_recognition.compare_faces([biden_encoding], unknown_encoding)
     faces = []
     for floc in face_locations:
@@ -46,14 +45,39 @@ while True:
         x1 = floc[3]
         y2 = floc[2]
         y1 = floc[0]
-        print(floc)
-        faces.append(frame[y1:y2, x1:x2])
-        frame = cv2.circle(frame, (x1, y1), 10, (0, 255, 0))
-        frame = cv2.circle(frame, (x2, y2), 10, (255, 0, 0))
+        # print(floc)
+        padding = 0
+        faces.append(frame[y1 - padding:y2 + padding, x1 - padding:x2 + padding])
+        # frame = cv2.circle(frame, (x1, y1), 10, (0, 255, 0))
+        # frame = cv2.circle(frame, (x2, y2), 10, (255, 0, 0))
 
+    indx = -1
     for face in faces:
+        indx +=1
+        face_encoding_success = False
+        try:
+            face_encoding = image_face_encodings[indx]
+            face_encoding_success = True
+        except:
+            pass
+        if face_encoding_success:
+            result = face_recognition.compare_faces(face_encodings, face_encoding)
+            if face_encodings == []:
+                print("No face encodings enter name for face")
+                name = input("name: ")
+                print(name)
+                face_encodings.append(face_encoding)
+                face_names.append(name)
+            else:
+                best_guess = np.argmax(result)
+                print(best_guess)
+
+            print("found face")
+            print(result)
+        else:
+            print("ERROR")
         # print(face.size)
-        cv2.imshow('Video', face)
+        cv2.imshow(str(indx), face)
 
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
